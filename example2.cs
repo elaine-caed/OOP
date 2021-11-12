095fffb08b070af2edd3ead06c046f8910568e93
elaine-caed <77505253+elaine-caed@users.noreply.github.com>
2021-11-12 10:56:35 +0200

    Add files via upload

---
+++ example2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication1
+{
+    class Example2
+    {
+        static void Main()
+        {
+            double p = 3.14159;
+            double x = 2.5;
+            double y = Math.Cos(p * x) / (1 + x * x);
+            Console.WriteLine();
+            Console.WriteLine(" x = {0} \t y = {1}", x, y);
+        }
+    }
+}
---
+++ example3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication1
+{
+    class Example2
+    {
+        static void Main(string[] args)
+        {
+            OperatingSystem os =
+                System.Environment.OSVersion;
+            Console.WriteLine("Platform: {0}", os.Platform);
+            System.Console.WriteLine("The current date and time is " + System.DateTime.Now);
+            System.Console.ReadLine();
+        }
+    }
+}
+
