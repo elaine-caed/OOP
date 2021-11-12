5f9f4ae7b4cf88e277df9eee0efbaaae12f7e0dd
elaine-caed <77505253+elaine-caed@users.noreply.github.com>
2021-11-12 11:48:03 +0200

    Add files via upload

---
+++ example1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab
+{
+    class Example1
+        {
+            static void Main()
+            {
+                Console.WriteLine("Hurray!\nToday is \"Computer science\"!!!");
+            }
+        }
+}
