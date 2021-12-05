b26e8fa06c7a2747f7d8621c1671441c534f2afe
elaine-caed <77505253+elaine-caed@users.noreply.github.com>
2021-12-05 15:27:23 +0200

    Add files via upload

---
+++ example10.cs
@@ -0,0 +1,16 @@
+﻿class State
+{
+    static State()
+    {
+        Console.WriteLine("Создано первое государство ");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        State s1 = new State(); // здесь сработает статический конструктор
+        State s2 = new State();
+        Console.ReadLine();
+    }
+}
